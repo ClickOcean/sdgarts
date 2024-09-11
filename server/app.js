273dfbcf8a4825ad9b9972346fa43e1b3a6555f6
Vladimir Vinogradov <vinograd6991@gmail.com>
2024-09-11 09:12:55 +0100

    Update version v3

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const corsOptions = {
-  origin: (callback) => callback(null, true),
+  origin: (origin, callback) => callback(null, true),
   maxAge: 3600,
   methods: ["GET", "HEAD", "OPTIONS", "POST", "PUT", "PATCH", "DELETE"],
 };
